7d16c24c05be61c9f4d8f1a4f821d26969969d26
suehyung <33367526+suehyung@users.noreply.github.com>
2018-01-10 00:00:16 -0800

    Fix typo to use PORT

--- BookmarkServer.py
+++ BookmarkServer.py
@@ -108,6 +108,6 @@ class Shortener(http.server.BaseHTTPRequestHandler):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8000))   # Use PORT if it's there.
-    server_address = ('', 8000)
+    server_address = ('', port)
     httpd = http.server.HTTPServer(server_address, Shortener)
     httpd.serve_forever()
